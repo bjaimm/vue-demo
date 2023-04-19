89f77f19763fa5c19919f56ace7a28735f569f13
HW <bjaimm@hotmail.com>
2023-04-19 13:41:14 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   devServer: {
 	  proxy: {
       '/api': {//代理标识，一般是每个接口前的相同部分
-        target: 'http://9.197.4.240:8888', // 这里写的是访问接口的域名和端口号
+        target: 'http://9.197.4.241:90', // 这里写的是访问接口的域名和端口号
         changeOrigin: true, // 允许跨域请求
         pathRewrite: { // 重写路径，替换请求地址中的指定路径
           '^/api': ''
